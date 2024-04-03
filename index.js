780c6b09dd9fe5c58a1fd88b593b0daf9f2389d0
kvgvmi <oucheikhimane7@gmail.com>
2024-04-03 03:48:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -122,3 +122,30 @@ function animate() {
 }
 animate()
 
+//--------------------------movment-----------------------------
+
+
+// hena kola key 3endha keycode dyalha . b7al a -> keycode:65
+addEventListener('keydown', ({keyCode}) => {
+    switch (keyCode){
+        case 65: 
+        console.log('left')
+        keys.left.pressed=true
+        break
+
+        case 83: 
+        console.log('down')
+        break
+
+        case 68: 
+        console.log('right')
+        keys.right.pressed=true
+        break
+
+        case 87: 
+        console.log('up')
+        player.velocity.y -= 20
+        break
+    }
+})
+

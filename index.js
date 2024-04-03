e61f465ab9efcd2bc72d3075fa9f7d22f3eb58fc
kvgvmi <oucheikhimane7@gmail.com>
2024-04-03 03:43:00 +0000

    Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas')//To select a spesific html element.
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth // the full width from the left to right of the window
+canvas.height = window.innerHeight
+
+const gravity=0.5
+

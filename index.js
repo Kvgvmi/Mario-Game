7f3bafc6cb0243933b36fab01b3baef34ec18a01
kvgvmi <oucheikhimane7@gmail.com>
2024-04-03 03:43:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ canvas.height = window.innerHeight
 
 const gravity=0.5
 
+class Player{
+    constructor(){
+        this.position={
+            x:100,
+            y:100
+        }
+
+        //gravity (to push player down)
+        this.velocity= {
+            x:0,
+            y:1
+        }
+        this.width=30
+        this.height=30
+    }
+
+    
+    //katwrina player kifach dayr
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
